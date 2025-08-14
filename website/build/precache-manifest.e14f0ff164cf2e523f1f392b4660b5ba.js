898bed1b188f4ea12256a1f77b703d9c9cfa9cc0
illusys <benakanbassey@gmail.com>
2025-08-14 14:38:23 +0100

    chore: pin Node 16 for Vercel and add build

---
+++ website/build/precache-manifest.e14f0ff164cf2e523f1f392b4660b5ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e726df97ba225f58f384ea463555c2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5710e358e6b65bb1cf66",
+    "url": "/static/css/main.719771e5.chunk.css"
+  },
+  {
+    "revision": "e8e064047f2dfb5186de",
+    "url": "/static/js/2.81549d76.chunk.js"
+  },
+  {
+    "revision": "677c07118884c7bf627d88f27d7f3f17",
+    "url": "/static/js/2.81549d76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5710e358e6b65bb1cf66",
+    "url": "/static/js/main.b9ac9922.chunk.js"
+  },
+  {
+    "revision": "52ba57a21bf9a5e30144",
+    "url": "/static/js/runtime-main.d2e54e43.js"
+  }
+]);
